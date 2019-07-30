69b8861727f6f45b5dc4490a1ae22dddc1c969d9
Caroline Setiawan <vc.setiawan@gmail.com>
2019-07-30 12:09:04 +1000

    Create test for counter
    Co-authored-by: Kasia Misirli <kasia.misirli@gmail.com>
    Co-authored-by: Jake Pitman <jakeseanp@gmail.com>

--- test/capture_web/router_test.exs
+++ test/capture_web/router_test.exs
@@ -56,15 +56,47 @@ defmodule CaptureWeb.RouterTest do
       response =
         Response
         |> Responses.for_survey(1)
-        |> IO.inspect(label: "before")
         |> Responses.for_question(1)
-        |> IO.inspect(label: "middle")
         |> Repo.one()
-        |> IO.inspect(label: "after")
 
       assert response.survey_id == 1
       assert response.question_id == 1
       assert response.disagree == 1
+    end
+  end
+
+  describe "with an updated response" do
+    setup do
+      %Response{survey_id: 1, question_id: 1, disagree: 1}
+      |> Repo.insert!()
+
+      :ok
+    end
+
+    test "updates the database with the correct response amount" do
+      params = %{
+        survey_id: 1,
+        question_id: 1,
+        previous_answer: 2,
+        updated_answer: 3
+      }
+
+      conn =
+        conn(:post, "/responses", params)
+        |> CaptureWeb.Router.call(@opts)
+
+      assert conn.status == 200
+
+      response =
+        Response
+        |> Responses.for_survey(1)
+        |> Responses.for_question(1)
+        |> Repo.one()
+
+      assert response.survey_id == 1
+      assert response.question_id == 1
+      assert response.disagree == 0
+      assert response.neutral == 1
     end
   end
 end
